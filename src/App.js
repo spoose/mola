837d8812fd1060795a84f20616abb25a7d050bf4
spoose <spooseageese@gmail.com>
2022-04-10 17:58:47 -0700

    local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import Questionnaire from "./components/questionarie";
+import Home from "./components/home";
+
+const App = () => {
+    return (
+        <div>
+            {/*<Navbar />*/}
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                {/*<Route exact path="/" element={<RecordList />} />*/}
+                {/*<Route path="/edit/:id" element={<Edit />} />*/}
+                {/*<Route path="/create" element={<Create />} />*/}
+                <Route path="/ques" element={<Questionnaire />} />
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
